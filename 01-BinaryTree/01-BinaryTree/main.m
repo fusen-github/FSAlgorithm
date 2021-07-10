504ac5aeeb143915cc72e484f6b17950227deb48
fusen <fusen@163.com>
2021-07-10 09:25:53 +0800

    初始化工程

---
+++ 01-BinaryTree/01-BinaryTree/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  01-BinaryTree
+//
+//  Created by 付森 on 2021/7/10.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
